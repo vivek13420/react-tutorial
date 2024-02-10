16225feaee1c9c8e07407aed8f8dc359854bdd74
vivek13420 <vivekrana13420@gmail.com>
2024-02-10 13:38:21 +0530

    added chnadfe

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <div>
       <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
+        <h1>React Essentias</h1>
         <p>
           Fundamental React concepts you will need for almost any app you are
           going to build!
